8efbc722ec821d4735fcb58c5ede238f4a9fc0e0
Петр <s.leonhart@mail.ru>
2022-07-05 20:10:00 +0300

    search-page done

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { MyDispatch } from '../../store';
 import { getBooksList } from '../../store/actions/searchPageAction';
+import "./SearchPage.css"
 
 function SearchPage() {
   const [searchValue, setSearchValue] = useState<string>("");
@@ -15,31 +16,34 @@ function SearchPage() {
   }
 
   return (
-    <div>
-      <form>
-        <label>
-          <input value={searchValue} onChange={(e) =>setSearchValue(e.target.value)}></input>
+    <div className='container'>
+      <form className='search-form'>
+        <h2 className='search-form__title'>Boo<span>k</span>s Search</h2>
+        <label className='search-form__input'>
+          <input  value={searchValue} onChange={(e) =>setSearchValue(e.target.value)}></input>
+          <button onClick={e => search(e)}>Search</button>
         </label>
-        <label>
-          <span>Categoties</span>
-          <select value={categories} onChange={(e) =>setCategories(e.target.value)}>
-            <option>All</option>
-            <option>Art</option>
-            <option>Biography</option>
-            <option>Computers</option>
-            <option>History</option>
-            <option>Medical</option>
-            <option>Poetry</option>
-          </select>
-        </label>
-        <label>
-          <span>SortingBy</span>
-          <select value={sortingBy} onChange={(e) =>setSortingBy(e.target.value)}>
-            <option>relevance</option>
-            <option>newest</option>
-          </select>
-        </label>
-        <button onClick={e => search(e)}>Искать</button>
+        <div>
+          <label className='search-form__filter'>
+            <span>Categoties</span>
+            <select className='search-form__filter_margin' value={categories} onChange={(e) =>setCategories(e.target.value)}>
+              <option>All</option>
+              <option>Art</option>
+              <option>Biography</option>
+              <option>Computers</option>
+              <option>History</option>
+              <option>Medical</option>
+              <option>Poetry</option>
+            </select>
+          </label>
+          <label className='search-form__filter'>
+            <span>Sorting by</span>
+            <select value={sortingBy} onChange={(e) =>setSortingBy(e.target.value)}>
+              <option>relevance</option>
+              <option>newest</option>
+            </select>
+          </label>
+        </div>
       </form>
     </div>
   );
